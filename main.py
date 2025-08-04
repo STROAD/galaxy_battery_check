e924e4cc29fdf078e12afa9df49065cd7f166c9a
STROAD <koseok0611@naver.com>
2025-08-04 21:40:59 +0900

    배터리 정보 가져오기

--- main.py
+++ main.py
@@ -2,4 +2,7 @@ from ppadb.client import Client as AdbClient
 
 
 client = AdbClient(host="127.0.0.1", port=5037)  # Default is "127.0.0.1" and 5037
-devices = client.devices()
+devices = client.devices()[0]
+
+battery_log = devices.shell("dumpsys battery")
+print(battery_log)
